bca439df9a00c621b28084414b7c8c2ad10073a2
Santiago <st147086@gmail.com>
2024-02-26 16:24:39 -0500

    Se hizo la página de acuerdo a los pasos del archivo JavaScript Ej

---
+++ js/contenido.js
@@ -0,0 +1,50 @@
+function nuevoParrafo() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<p>' + texto + '</p>';
+}
+
+function nuevoTitulo() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<h1>' + texto + '</h1>';
+}
+
+function nuevoLink() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    var atributos = texto.split(' ')
+
+
+    contenido.innerHTML = contenido.innerHTML + `<a href="${atributos[1]}" target="_blank">` + atributos[0] + '</a>';
+}
+
+document.getElementById('imagen').onchange = function (evt) {
+    var entradaArchivo = evt.target,
+        archivos = entradaArchivo.files;
+    if (FileReader && archivos && archivos.length) {
+        var imagen = new FileReader();
+        imagen.onload = function () {
+            var etiquetaImg = document.createElement("img");
+            etiquetaImg.setAttribute('src', imagen.result);
+            etiquetaImg.setAttribute('width', '200px');
+            etiquetaImg.setAttribute('height', '180px');
+            var contenido = document.getElementById('contenido');
+            contenido.appendChild(etiquetaImg);
+        }
+        imagen.readAsDataURL(archivos[0]);
+    }
+}
+
+function imprimir() {
+    var divContents = document.getElementById("contenido").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.document.write('<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />');
+    a.document.write('<body > <h1>Resultado</h1> <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
+
